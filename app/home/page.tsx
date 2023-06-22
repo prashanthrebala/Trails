2000ced7b2ac2fcde6b20039972a0533336a5af7
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-21 23:08:49 -0400

    home page

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div className="w-full bg-amber-500 min-h-screen fx-center">
+			<div className="w-1/3"></div>
+		</div>
+	);
+};
+
+export default Home;
