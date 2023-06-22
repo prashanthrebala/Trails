f568093933fdbdeb40e8b8854ec8c1d02789fe80
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-22 00:52:53 -0400

    dummy data

---
+++ constants/index.ts
@@ -0,0 +1,46 @@
+export const posts = [
+	{
+		username: "Prashanth Rebala",
+		likes: 0,
+		imageURL:
+			"https://ik.imagekit.io/prashanthrebala/trail/292448076_417272036999820_8867583513797257827_n.jpg?updatedAt=1687404715599",
+		caption: "Lorem Ipsum",
+		location: {
+			latitude: 40.7538,
+			longitude: -74.0022,
+		},
+	},
+	{
+		username: "Alpha Charlie",
+		likes: 0,
+		imageURL:
+			"https://ik.imagekit.io/prashanthrebala/trail/20230318_182123.jpg?updatedAt=1687404716425",
+		caption: "Dolor sit amet",
+		location: {
+			latitude: 40.7538,
+			longitude: -74.0022,
+		},
+	},
+	{
+		username: "Bravo Echo",
+		likes: 0,
+		imageURL:
+			"https://ik.imagekit.io/prashanthrebala/trail/290706291_736675464249562_6323937530762778973_n.jpg?updatedAt=1687404715690",
+		caption: "Consectetur adipiscing",
+		location: {
+			latitude: 40.7538,
+			longitude: -74.0022,
+		},
+	},
+	{
+		username: "Mike Zulu",
+		likes: 0,
+		imageURL:
+			"https://ik.imagekit.io/prashanthrebala/trail/262745676_267537225346689_7788439608521136332_n.jpg?updatedAt=1687404716032",
+		caption: "Duis aute",
+		location: {
+			latitude: 40.7538,
+			longitude: -74.0022,
+		},
+	},
+];
