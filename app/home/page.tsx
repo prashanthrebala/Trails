8c572e9397e63071d2cc3763e83716ac76188b0a
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-22 00:53:58 -0400

    display all posts

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { ImageCard } from "@/components";
+import { ImageCardProps } from "@/types";
+import { posts } from "@/constants";
 
 const Home = () => {
 	return (
 		<div className="w-full bg-amber-500 min-h-screen fx-center">
-			<div className="w-1/3"></div>
+			<div className="w-1/3">
+				{posts.map((post: ImageCardProps) => (
+					<ImageCard
+						username={post.username}
+						imageURL={post.imageURL}
+						caption={post.caption}
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
