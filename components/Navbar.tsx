b0470b3cf7385c15146f9830dee895336ec6f69c
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-21 23:08:32 -0400

    init components folder with Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<div className="w-full h-14 bg-green-600 fx-center">
+			<div className="w-20 fx-center">Trail</div>
+			<div className="grow fx-center">
+				<input className="w-full md:w-1/2 max-w-xl h-10 rounded-full" />
+			</div>
+			<div className="w-20 fx-center">Profile</div>
+		</div>
+	);
+};
+
+export default Navbar;
