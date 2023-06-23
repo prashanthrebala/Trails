5295e576890a0cd38d5e9f2b075a8e6c39368cfc
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-23 02:48:09 -0400

    fetch data from api in home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,7 +3,17 @@ import { ImageCard } from "@/components";
 import { ImageCardProps } from "@/types";
 import { posts } from "@/constants";
 
-const Home = () => {
+const getPosts = async () => {
+	const postApiURL = "http://localhost:3000/api/posts";
+	const response = await fetch(postApiURL);
+	const responseData = await response.json();
+	const postsData = responseData.data;
+	return postsData;
+};
+
+const Home = async () => {
+	const posts = await getPosts();
+
 	return (
 		<div className="w-full bg-amber-500 min-h-screen fx-center">
 			<div className="w-full max-w-3xl">
