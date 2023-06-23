497ff447463aa522de1c33a478890c2b39b8c3a7
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-23 18:25:33 -0400

    POST route set up for posts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,4 +1,5 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
 import clientPromise from "@/lib/mongo";
 
 export async function GET() {
@@ -6,4 +7,43 @@ export async function GET() {
 	const db = await mongoClient.db("trail");
 	const data = await db.collection("posts").find().toArray();
 	return NextResponse.json({ data });
+}
+
+export async function POST(req: Request) {
+	const data = await req.json();
+	console.log("YOLO ALL");
+	console.log(typeof data.fileBase64);
+	const result = await fetch("http://localhost:5109/", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			image64: data.fileBase64,
+		}),
+	});
+	console.log("YOLO AGAIN ALL");
+	const responseData = await result.json();
+	console.log(responseData);
+	// console.log(req.method);
+	// console.log(req.body);
+	return NextResponse.json({ data: "hello world" });
+
+	// try {
+	// 	const formData = await req.formData();
+	// 	const caption = formData.get("caption");
+	// 	const location = formData.get("location");
+	// 	const file = formData.get("myImage");
+
+	// 	console.log(caption);
+	// 	console.log(location);
+
+	// 	// Handle the data as needed
+	// 	// ...
+
+	// 	return NextResponse.json({ data: "hello world" });
+	// } catch (error) {
+	// 	console.error("Error parsing request body:", error);
+	// 	return NextResponse.error();
+	// }
 }
