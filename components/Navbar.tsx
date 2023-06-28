b2b51d00286d7d1805175a36c47d9590168bfe21
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-28 00:42:05 -0400

    changed colors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<div className="w-full h-14 bg-green-600 fx-center">
+		<div className="w-full h-16 bg-gray-950 text-neutral-100 fx-center">
 			<div className="w-20 fx-center">Trails</div>
 			<div className="grow fx-center">
 				<input className="w-full md:w-1/2 max-w-xl h-10 rounded-full" />
