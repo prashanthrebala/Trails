87ebc4d7ba595b16f36627e904883d38027c8a03
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-02 17:48:12 -0400

    header input styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,10 @@ const Navbar = () => {
 		<div className="w-full h-16 bg-gray-950 text-neutral-100 fx-center">
 			<div className="w-20 fx-center">Trails</div>
 			<div className="grow fx-center">
-				<input className="w-full md:w-1/2 max-w-xl h-10 rounded-full" />
+				<input
+					className="w-full md:w-1/2 max-w-xl h-10 rounded-full outline-none text-neutral-900 px-8"
+					placeholder="Search for Trails"
+				/>
 			</div>
 			<div className="w-20 fx-center">Profile</div>
 		</div>
