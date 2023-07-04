a4d92e722d30bdc75a748059995413c1791bc9ee
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-04 17:00:21 -0400

    auth notes for personal use

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
+// NOTES for NextAuth
+// all authOptions are available at api/auth/signin by default
+// you can redirect them to this route when session isn't available
+// alternatively, you can create your own sign in page
+// you can redirect your user to your custom sign in page
+// each sign in button will have a callback with signIn("authProvider", {callbackURL?})
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
