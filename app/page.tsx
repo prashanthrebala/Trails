01208ac0234efe978dceb02d6bf2003c030738ce
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-04 17:00:05 -0400

    created Session Context in contexts folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import NextAuthSessionProvider from "./providers/sessionProvider";
-
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<NextAuthSessionProvider>Root page</NextAuthSessionProvider>
+		<main className="flex flex-col items-center justify-between p-24 text-5xl font-bold text-neutral-100">
+			Root Page
 		</main>
 	);
 }
--- contexts/SessionContext.tsx
+++ contexts/SessionContext.tsx
@@ -3,6 +3,10 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 
-export default function NextAuthSessionProvider({ children }: any) {
+export default function SessionContext({
+	children,
+}: {
+	children?: React.ReactNode;
+}) {
 	return <SessionProvider>{children}</SessionProvider>;
 }
