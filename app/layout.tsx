782a4ebc00d05ce3682508ab82ee8d43edf9c152
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-06 14:47:28 -0400

    removed fonts from page root

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
 import { Navbar } from "@/components";
 import bgImage from "@/public/background/freepik-forest.jpg";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
 	title: "Trails",
