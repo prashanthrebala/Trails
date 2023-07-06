36e793a82c3af471547ffa3e8bfc956976eb73a0
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-06 14:47:38 -0400

    modified landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Oxygen } from "next/font/google";
+const inter = Oxygen({ weight: "700", subsets: ["latin"] });
+
 export default function Home() {
 	return (
-		<main className="flex flex-col items-center justify-between p-24 text-5xl font-bold text-neutral-100">
-			Root Page
-		</main>
+		<div className="w-full h-full flex items-center">
+			<div
+				className={`leading-10 px-4 lg:ml-32 w-full lg:w-1/2 lg:h-1/2 text-zinc-100 text-4xl lg:text-6xl flex items-center ${inter.className}`}
+			>
+				Uncover popular tourist attractions to visit from your next travel
+				destination
+			</div>
+		</div>
 	);
 }
