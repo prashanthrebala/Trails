51c262674d0245038f6f193e7a7764098ef88a32
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-29 11:36:15 -0400

    implemented scroll end loader

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { ImageCard } from "@/components";
 import { ImageCardProps } from "@/types";
 import { posts } from "@/constants";
+import ScrollEndComponent from "@/components/ScrollEndComponent";
 
-const getPosts = async () => {
-	const postApiURL = "http://192.168.1.129:3000/api/posts";
+const getPosts = async (page: number) => {
+	const postApiURL = "http://192.168.1.129:3000/api/posts?page=" + page;
 	const response = await fetch(postApiURL, {
 		next: { revalidate: 10 },
 	});
@@ -12,9 +14,18 @@ const getPosts = async () => {
 	const postsData = responseData.data;
 	return postsData;
 };
+const Home = () => {
+	const [page, setPage] = useState(0);
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			const postsResponse = await getPosts(page);
+			setPosts([...posts, ...postsResponse] as any);
+			console.log("updated");
+		};
 
-const Home = async () => {
-	const posts = await getPosts();
+		fetchData();
+	}, [page]);
 
 	return (
 		<div className="w-full min-h-screen fx-center bg-[#00000000]">
@@ -28,6 +39,7 @@ const Home = async () => {
 						addCropRatio={post.addCropRatio}
 					/>
 				))}
+				<ScrollEndComponent setPage={setPage} shouldFetch={posts.length > 0} />
 			</div>
 		</div>
 	);
